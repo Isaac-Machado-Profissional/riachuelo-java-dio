ff499c1873ad4395a35bdc5c36672e3433d0e4a6
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-08-21 14:37:26 -0300

    feat: heranca e polimorfismo

---
+++ curso-2_poo-e-estrutura-de-dados-com-java/modulo2_classes-encapsulamento/secao-2/a1-heranca-e-polimorfismo/src/Client.java
@@ -0,0 +1,15 @@
+public class Client {
+
+    private String document;
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+
+
+}
---
+++ curso-2_poo-e-estrutura-de-dados-com-java/modulo2_classes-encapsulamento/secao-2/a1-heranca-e-polimorfismo/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee manager = new Manager();
+
+        manager.setName("João");
+//        manager.setLogin("joao");
+//        manager.setPassword("123456");
+//
+//        System.out.println(manager.getName());
+//        System.out.println(manager.getLogin());
+//        System.out.println(manager.getPassword());
+
+
+
+    }
+}
